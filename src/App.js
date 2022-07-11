6f11114d0e590ffb87c183f009a6b3401830fa3f
Thompson Tong <thompsontong@Thompsons-MacBook-Pro.local>
2022-07-11 12:12:46 +0800

    Redesigned

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home";
-import Gym from "./routes/gym/gym";
-import SixDayWorkout from "./routes/gym/six_day_workout/sixDayWorkout";
-import Evo from "./routes/gym/evo/evo";
-import Music from "./routes/music/music";
-import Clothing from "./routes/clothing/clothing";
-import Programming from "./routes/programming/programming";
 import "./App.css";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
-export default function App() {
+function App() {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/gym" element={<Gym />} />
-                    <Route path="/gym/six_day_workout" element={<SixDayWorkout />} />
-                    <Route path="/gym/evo" element={<Evo />} />
-                    <Route path="/clothing" element={<Clothing />} />
-                    <Route path="/music" element={<Music />} />
-                    <Route path="/programming" element={<Programming />} />
-                    {/* <Route
-                        path="/profile"
-                        element={<Profile value={"VALUE STATED IN INDEX.JS"} />}
-                    /> */}
-                </Routes>
-            </BrowserRouter>
+            <div className="text-center flex fullscreen">
+                <div className="flex-item">
+                    <p>
+                        t-industries [ti] is not an organization. no rights
+                        reserved.
+                    </p>
+                </div>
+                <div className="flex-item">
+                        <h1>coming soon™</h1>
+                </div>
+            </div>
         </>
     );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createRoot } from 'react-dom/client';
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-const rootElement = document.getElementById("root");
-createRoot(rootElement).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <>
+        <App />
+    </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
