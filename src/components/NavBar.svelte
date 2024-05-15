f542332bdab6a539d21e5dcdde3f5bd60f072fb9
thompson <hi.thompson@hotmail.com>
2024-05-15 21:28:42 +0100

    build(gh-pages): set up

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import NavItem from "./NavItem.svelte";
+    import { base } from "$app/paths";
 </script>
 
 <div class="flex justify-center border p-1 mt-12">
     <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center">
-        <NavItem href="/" text="Home" />
+        <NavItem href="{base}/" text="Home" />
         <NavItem href="#" text="Blog" /> <!-- TODO: href -->
-        <NavItem href="/projects" text="Projects" />
-        <NavItem href="/ethos" text="&et" />
+        <NavItem href="{base}/projects" text="Projects" />
+        <NavItem href="{base}/ethos" text="&et" />
     </ul>
 </div>
