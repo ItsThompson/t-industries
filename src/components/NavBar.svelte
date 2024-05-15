6e74a6629f2758defa78cc12b4e634e841f4e4b4
thompson <hi.thompson@hotmail.com>
2024-05-15 21:42:35 +0100

    feat(blog): removed blog from nav

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -6,7 +6,7 @@
 <div class="flex justify-center border p-1 mt-12">
     <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center">
         <NavItem href="{base}/" text="Home" />
-        <NavItem href="#" text="Blog" /> <!-- TODO: href -->
+        <!-- TODO: <NavItem href="#" text="Blog" /> -->
         <NavItem href="{base}/projects" text="Projects" />
         <NavItem href="{base}/ethos" text="&et" />
     </ul>
