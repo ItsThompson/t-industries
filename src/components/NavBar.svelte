81ca78d3a74ab424f37f658ece97063b5fa1d208
thompson <hi.thompson@hotmail.com>
2024-08-06 15:39:17 +0800

    feat(navbar): Added blog link

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -6,8 +6,8 @@
 <div class="flex justify-center border p-1 mt-12">
     <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center">
         <NavItem href="{base}/" text="Home" />
-        <!-- TODO: <NavItem href="#" text="Blog" /> -->
         <NavItem href="{base}/projects" text="Projects" />
+        <NavItem href="https://blog.t-industri.es" text="Blog" />
         <NavItem href="{base}/ethos" text="&et" />
     </ul>
 </div>
