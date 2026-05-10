be3ae7743f00f025a3ecdcce55b9f9aee2d4ac69
Thompson Tong <thompsnt@amazon.co.uk>
2026-05-10 15:13:36 +0100

    feat: add /experience page route with timeline
    Wider container (md:w-3/4 lg:w-3/5) to accommodate alternating timeline
    layout. Imports Timeline component and static experiences data.

---
+++ src/routes/experience/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Timeline from '../../components/Timeline.svelte';
+	import { experiences } from '$lib/experiences';
+</script>
+
+<div class="w-11/12 md:w-3/4 lg:w-3/5 2xl:w-1/2 border bg-black p-5 flex flex-col items-center gap-5">
+	<div class="self-start w-full">
+		<h4 class="text-xl font-bold underline">Experience</h4>
+		<div class="mt-5">
+			<Timeline {experiences} />
+		</div>
+	</div>
+</div>
