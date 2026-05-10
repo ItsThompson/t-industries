ed95bdb30d3a43ceb44d4a234008d2a690f36c50
Thompson Tong <thompsnt@amazon.co.uk>
2026-05-10 15:13:44 +0100

    feat: add Experience nav item to navbar
    Placed between Home and Projects for logical navigation flow.

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -6,6 +6,7 @@
 <div class="flex justify-center border bg-black p-1 mt-12">
     <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center">
         <NavItem href="{base}/" text="Home" />
+        <NavItem href="{base}/experience" text="Experience" />
         <NavItem href="{base}/projects" text="Projects" />
         <NavItem href="{base}/ethos" text="&et" />
     </ul>
