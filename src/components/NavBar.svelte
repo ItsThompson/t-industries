94412188d422a6bf6f8f8629c81785e747636309
Thompson Tong <thompsnt@amazon.co.uk>
2026-05-11 19:21:28 +0100

    feat: restyle NavBar with industrial border and add Manifest link
    - Add 'Manifest' NavItem with href to /manifest
    - Change border to border-secondary-200 for industrial aesthetic
    - Add flex-wrap to prevent nav items clipping on mobile

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,11 +3,12 @@
     import { base } from "$app/paths";
 </script>
 
-<div class="flex justify-center border bg-black p-1 mt-12">
-    <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center">
+<div class="flex justify-center border border-secondary-200 bg-black p-1 mt-12">
+    <ul class="flex sm:flex-row flex-col p-2 sm:space-x-8 items-center flex-wrap">
         <NavItem href="{base}/" text="Home" />
         <NavItem href="{base}/experience" text="Experience" />
         <NavItem href="{base}/projects" text="Projects" />
         <NavItem href="{base}/ethos" text="&et" />
+        <NavItem href="{base}/manifest" text="Manifest" />
     </ul>
 </div>
